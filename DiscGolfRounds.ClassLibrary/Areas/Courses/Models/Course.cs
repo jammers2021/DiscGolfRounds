c95d396a8c1f3dd4cfaf07eab38be6db7cecbbde
jammers2021 <jammers2021@gmail.com>
2023-03-31 09:55:03 -0400

    Models Created

---
+++ DiscGolfRounds.ClassLibrary/Areas/Courses/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscGolfRounds.ClassLibrary.Areas.Courses.Models
+{
+    internal class Course
+    {
+    }
+}
